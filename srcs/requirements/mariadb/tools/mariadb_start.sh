b0c00b72aea0acf93fd97484a9b7ab3d1dca78ba
adesgran <adesgran.dev@gmail.com>
2023-04-13 08:54:41 -0400

    WIP

--- srcs/requirements/mariadb/tools/mariadb_start.sh
+++ srcs/requirements/mariadb/tools/mariadb_start.sh
@@ -2,11 +2,25 @@
 
 if [ ! -f /conf ]
 then
-        echo "status"
-        mysqld status
+        mkdir -p /run/mysqld
+        chown -R mysql:mysql /run/mysqld
+        ls /var/run/mysqld
         echo ""
+       # mysqladmin -uroot -p ${DB_PASS}
         echo "mysqld &"
-        mysqld
+        mysqld &
+        while !(mysqladmin ping > /dev/null)
+	do
+		echo "Waiting for database to be ready..." 
+	    sleep 5
+	done
+
+        mysql -uroot --skip-password -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ROOT';"
+	mysql -uroot -p$DB_ROOT -e "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PASS';"
+	mysql -uroot -p$DB_ROOT -e "GRANT ALL PRIVILEGES ON *.* TO '$DB_USER'@'%' IDENTIFIED BY '$DB_PASS';"
+	mysql -uroot -p$DB_ROOT -e "CREATE DATABASE IF NOT EXISTS $DB_NAME;"
+	mysql -uroot -p$DB_ROOT -e "FLUSH PRIVILEGES;"
+
 
         touch /conf
 else
