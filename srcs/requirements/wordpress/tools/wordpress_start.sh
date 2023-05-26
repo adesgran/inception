54f17b8a26f585ba5fb47f42f855bbcddcbf2da8
adesgran <adesgran@Inception.inception.virtualbox.org>
2023-05-26 11:09:54 +0200

    wip

--- srcs/requirements/wordpress/tools/wordpress_start.sh
+++ srcs/requirements/wordpress/tools/wordpress_start.sh
@@ -5,18 +5,19 @@ then
         rm -f /var/www/wp-config.php
 
         echo "wp core download"
-        wp core download --allow-root --path="/var/www"
+        wp core download --allow-root --path="/var/www/"
 
         echo "wp config create"
-        wp config create --allow-root --dbname="$DB_NAME" --dbuser="$DB_USER" --dbpass="$DB_PASS" --dbhost="$DB_HOST" --path="/var/www"
+        wp config create --allow-root --dbname="$DB_NAME" --dbuser="$DB_USER" --dbpass="$DB_PASS" --dbhost="$DB_HOST" --path="/var/www/"
 
         echo "wp core install"
         wp core install --allow-root --admin_user="$ADMIN_USER" --admin_password="$ADMIN_PWD"\
-                --admin_email="$ADMIN_MAIL" --url="${URL}/wordpress" --title="Inception" --path="/var/www"
+                --admin_email="$ADMIN_MAIL" --url="${URL}/wordpress" --title="Inception" --path="/var/www/"
 
         echo "wp user create"
         wp user create --allow-root "$WP_USER" "$WP_USER_MAIL" --role="author"\
-                --user_pass="$WP_USER_PWD" --path="/var/www"
+                --user_pass="$WP_USER_PWD" --path="/var/www/"
+
 
         touch /conf
 else
